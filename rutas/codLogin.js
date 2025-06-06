c4da5f06913ba53c4beba9be08d30ddc94880930
unknown <eduarjosesuarezgraterol@gmail.com>
2025-06-06 09:57:34 -0400

    v1.1

--- rutas/codLogin.js
+++ rutas/codLogin.js
@@ -6,7 +6,7 @@ const conexion = require('../config/conexion');
 router.post('/codLogin', function(req, res){
     const {user, pass} = req.body;
 
-    const validar = `SELECT * FROM inf_usuarios WHERE usuario = '${user}'`;
+    const validar = `SELECT * FROM inf_usuarios WHERE usuario = '${user}' AND clave = '${pass}'`;
     conexion.query(validar, async function(error, rows){
         let mensaje;
         if (error) {
@@ -19,33 +19,35 @@ router.post('/codLogin', function(req, res){
             return res.render('login', { mensaje, link });
         }
 
-        const user = rows[0];
+        const users = rows[0];
 
-        if (!pass || !user.pass) {
+        if (!pass || !users.clave) {
             mensaje = "Contraseña incorrecta";
             return res.render('login', { mensaje, link });
         }
 
         // Almacenar datos en sesión (sin clave)
         req.session.login = true;
-        req.session.id = user.id;
-        req.session.nombre_A = user.nombre_apellido;
-        req.session.usuario = user.usuario;
-        req.session.email = user.email;
-        req.session.telefono = user.telefono;
-        req.session.cedula = user.cedula;
-        req.session.edificio = user.edificio;
-        req.session.apartamento = user.apartamento;
-        req.session.id_cargo = user.id_cargo;
-        req.session.fecha = user.fecha;
+        req.session.id = users.id;
+        req.session.nombre_A = users.nombre_apellido;
+        req.session.usuario = users.usuario;
+        req.session.email = users.email;
+        req.session.telefono = users.telefono;
+        req.session.cedula = users.cedula;
+        req.session.edificio = users.edificio;
+        req.session.apartamento = users.apartamento;
+        req.session.id_cargo = users.id_cargo;
+        req.session.fecha = users.fecha;
 
         console.log(req.session);
 
-        if (user.id_cargo === 1) {
+        if (users.id_cargo === 1) {
             return res.redirect('/admin');
-        } else if (user.id_cargo === 2) {
+        } 
+        else if (users.id_cargo === 2) {
             return res.redirect('/users');
-        } else {
+        }
+        else {
             mensaje = "Rol de usuario no válido";
             return res.render('login', { mensaje, link });
         }
